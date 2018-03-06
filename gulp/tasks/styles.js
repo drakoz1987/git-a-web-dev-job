dd1869a3d27aa6e496b2b8d8591538b8acf1914c
drakoz1987 <drakoz1987@gmail.com>
2018-03-06 08:04:25 -0400

    Advanced in the projects header

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    cssimport = require('postcss-import'),
+    cssvars = require('postcss-simple-vars'),
+    autoprefixer = require('autoprefixer'),
+    nested = require('postcss-nested'),
+    mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+        .on('error', (errorInfo) => {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        }).pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+
+
+gulp.task('watch', () => {
+
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', () => {
+        browserSync.reload();
+    });
+    watch('./app/assets/styles/**/*.css', () => {
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'], () => {
+    return gulp.src('./app/assets/styles/styles.css').pipe(browserSync.stream());
+});
