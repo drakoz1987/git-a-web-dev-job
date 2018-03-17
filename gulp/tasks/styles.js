94820a40841f633868266958f922aeb83492c0a8
drakoz1987 <drakoz1987@gmail.com>
2018-03-16 20:16:09 -0400

    Finally merged everything

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     autoprefixer = require('autoprefixer'),
     nested = require('postcss-nested'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer, hexrgba]))
         .on('error', (errorInfo) => {
             console.log(errorInfo.toString());
             this.emit('end');
