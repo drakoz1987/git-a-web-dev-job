65fce9d945b5e5dbf7bb8967636fe64ecd45c37a
drakoz1987 <drakoz1987@gmail.com>
2018-04-03 11:34:59 -0400

    Finished working on the modal window

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+
+class Modal {
+    constructor() {
+        this.openMopdalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.events();
+    }
+    events() {
+        //clicking the open modal button
+        this.openMopdalButton.click(this.openModal.bind(this))
+            //clicking the x close the modal
+        this.closeModalButton.click(this.closeModal.bind(this))
+            //pushes any key
+        $(document).keyup(this.keyPressHandler.bind(this));
+
+    }
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeModal();
+        }
+    }
+    openModal() {
+        this.modal.addClass("modal--is-visible");
+        return false;
+
+    }
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+        return false;
+    }
+
+}
+export default Modal;
